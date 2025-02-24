aa7d125da68a418970ac6f3526978b37a7ed34b2
Edward <emendieta90@gmail.com>
2025-02-23 22:19:13 -0500

    not really sure at this point

--- private/database.php
+++ private/database.php
@@ -4,6 +4,7 @@
 
     function db_connect() {
        $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+       confirm_db_connect();
        return $connection; 
     }
     function db_disconnect($connection) {
@@ -11,4 +12,17 @@
             mysqli_close($connection);
         }
     }
+    function confirm_db_connect() {
+        if(mysqli_connect_errno()) {
+          $msg = "Database connection failed: ";
+          $msg .= mysqli_connect_error();
+          $msg .= " (" . mysqli_connect_errno() . ")";
+          exit($msg);
+        }
+      }
+      function confirm_result_set($result_set) {
+        if(!$result_set) {
+            exit("Database query failed.");
+        }
+      }
 ?>
--- private/query_functions.php
+++ private/query_functions.php
@@ -1,9 +1,11 @@
 <?php
 function find_all_subjects() {
     global $db;
-    $sql = "SELECT * FROM subjects ";
+    $sql = "SELECT * FROM Subjects ";
     $sql .= "ORDER BY position ASC";
+    //echo $sql;
     $result = mysqli_query($db, $sql);
+    confirm_result_set($result);
     return $result;
 }
     

5d2c23df4fddedfe7c0bbb11f7528d23b0c256ba
Edward <emendieta90@gmail.com>
2025-02-24 20:43:37 -0500

    Check position now updates

--- public/staff/subjects/create.php
+++ public/staff/subjects/create.php
@@ -3,11 +3,12 @@
 require_once('../../../private/initialize.php');
 
 if(isPostRequest()) {
-    $menu_name = $_POST['menu_name'] ?? '';
-    $position = $_POST['position'] ?? '';
-    $visible = $_POST['visible'] ?? '';
+    $subject = [];
+    $subject['menu_name'] = $_POST['menu_name'] ?? '';
+    $subject['position'] = $_POST['position'] ?? '';
+    $subject['visible'] = $_POST['visible'] ?? '';
 
-    $result = insert_subject($menu_name, $position, $visible);
+    $result = insert_subject($subject);
     $new_id = mysqli_insert_id($db);
     redirect_to(url_for('subjects/show.php?id=' . $new_id));
     
--- public/staff/subjects/edit.php
+++ public/staff/subjects/edit.php
@@ -21,6 +21,10 @@ if(isPostRequest()) {
 
 } else {
     $subject = find_subject_by_id($id);
+
+    $subject_set = find_all_subjects();
+    $subject_count = mysqli_num_rows($subject_set);
+    mysqli_free_result($subject_set);
 }
 ?>
 
@@ -42,7 +46,15 @@ if(isPostRequest()) {
                 <dt>Position</dt>
                 <dd>
                     <select name="position">
-                        <option value="1"<?php if($subject['position'] == "1") {echo "selected"; }?>>1</option>
+                        <?php 
+                            for($i=1; $i<=$subject_count; $i++) {
+                                echo "<option value=\"{$i}\"";
+                                if($subject["position"] == $i) {
+                                    echo " selected";
+                                }
+                                echo ">{$i}</option>";
+                            }
+                        ?>
                     </select>
                 </dd>
             </dl>

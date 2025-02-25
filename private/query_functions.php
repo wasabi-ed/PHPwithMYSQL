02562815e84e0412a9de3e1b51a6d748d8c53831
Edward <emendieta90@gmail.com>
2025-02-24 22:10:29 -0500

    added CRUD functionality to Pages directory

--- private/query_functions.php
+++ private/query_functions.php
@@ -37,15 +37,6 @@ function insert_subject($subject) {
         exit;
     }
 }
-function find_all_pages() {
-    global $db;
-
-    $sql = "SELECT * FROM Pages ";
-    $sql .= "ORDER BY subject_id ASC, position ASC";
-    $result = mysqli_query($db, $sql);
-    confirm_result_set($result);
-    return $result;
-}
 function update_subject($subject) {
     global $db;
 
@@ -82,6 +73,90 @@ function delete_subject($id) {
         echo mysqli_error($db);
         db_disconnect($db);
         exit;
+    }
+}
+function find_all_pages() {
+    global $db;
+
+    $sql = "SELECT * FROM Pages ";
+    $sql .= "ORDER BY subject_id ASC, position ASC";
+    $result = mysqli_query($db, $sql);
+    confirm_result_set($result);
+    return $result;
+}
+function find_page_by_id($id) {
+    global $db;
+    $sql = "SELECT * FROM Pages ";
+    $sql .= "WHERE id = '" . $id . "'";
+    $result = mysqli_query($db, $sql);
+    confirm_result_set($result);
+    $pages = mysqli_fetch_assoc($result);
+    mysqli_free_result($result);
+    return $pages;
+}
+function insert_page($page) {
+    global $db;
+
+    $sql = "INSERT INTO pages ";
+    $sql .= "(subject_id, menu_name, position, visible, content) ";
+    $sql .= "VALUES (";
+    $sql .= "'" . $page['subject_id'] . "',";
+    $sql .= "'" . $page['menu_name'] . "',";
+    $sql .= "'" . $page['position'] . "',";
+    $sql .= "'" . $page['visible'] . "',";
+    $sql .= "'" . $page['content'] . "'";
+    $sql .= ")";
+    $result = mysqli_query($db, $sql);
+    // For INSERT statements, $result is true/false
+    if($result) {
+      return true;
+    } else {
+      // INSERT failed
+      echo mysqli_error($db);
+      db_disconnect($db);
+      exit;
+    }
+}
+function update_page($page) {
+    global $db;
+
+    $sql = "UPDATE pages SET ";
+    $sql .= "subject_id='" . $page['subject_id'] . "', ";
+    $sql .= "menu_name='" . $page['menu_name'] . "', ";
+    $sql .= "position='" . $page['position'] . "', ";
+    $sql .= "visible='" . $page['visible'] . "', ";
+    $sql .= "content='" . $page['content'] . "' ";
+    $sql .= "WHERE id='" . $page['id'] . "' ";
+    $sql .= "LIMIT 1";
+
+    $result = mysqli_query($db, $sql);
+    // For UPDATE statements, $result is true/false
+    if($result) {
+      return true;
+    } else {
+      // UPDATE failed
+      echo mysqli_error($db);
+      db_disconnect($db);
+      exit;
+    }
+
+}
+function delete_page($id) {
+    global $db;
+
+    $sql = "DELETE FROM pages ";
+    $sql .= "WHERE id='" . $id . "' ";
+    $sql .= "LIMIT 1";
+    $result = mysqli_query($db, $sql);
+
+    // For DELETE statements, $result is true/false
+    if($result) {
+      return true;
+    } else {
+      // DELETE failed
+      echo mysqli_error($db);
+      db_disconnect($db);
+      exit;
     }
 }
 ?>

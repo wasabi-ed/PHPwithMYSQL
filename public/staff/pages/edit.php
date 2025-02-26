f6657a1aa1c72a75855f55aa5afa8aa3a0d7fe4c
Edward <emendieta90@gmail.com>
2025-02-26 11:45:47 -0500

    validate pages

--- public/staff/pages/edit.php
+++ public/staff/pages/edit.php
@@ -21,17 +21,17 @@ if(isPostRequest()) {
     $page['content'] = $_POST['content'] ?? '';
 
     $result = update_page($page);
-    redirect_to(url_for('staff/pages/show.php?id=' . $id));
-
+    if($result === true) {
+        redirect_to(url_for('staff/pages/show.php?id=' . $id));
+    } else {
+        $errors = $result;
+    }
 } else {
-
   $page = find_page_by_id($id);
-
-  $page_set = find_all_pages();
-  $page_count = mysqli_num_rows($page_set);
-  mysqli_free_result($page_set);
-
 }
+$page_set = find_all_pages();
+$page_count = mysqli_num_rows($page_set);
+mysqli_free_result($page_set);
 ?>
 
 <?php $page_title = 'Edit Page'; ?>
@@ -43,6 +43,7 @@ if(isPostRequest()) {
     <div class="page edit">
         <h1>Edit Page</h1>
 
+        <?php echo display_errors($errors); ?>
         <form action="<?php echo url_for('staff/pages/edit.php?id=' . h(u($id))); ?>" method="POST">
             <dl>
                 <dt>Subject</dt>

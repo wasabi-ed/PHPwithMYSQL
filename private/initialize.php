f943a8d4c9fb61edfcee059f9ad96f67389dff24
Edward <emendieta90@gmail.com>
2025-02-26 10:03:59 -0500

    display validation errors

--- private/initialize.php
+++ private/initialize.php
@@ -15,5 +15,6 @@
     require_once('validation_functions.php');
 
     $db = db_connect();
+    $errors = [];
 
 ?>
--- public/staff/subjects/new.php
+++ public/staff/subjects/new.php
@@ -1,6 +1,25 @@
 <?php
 
 require_once('../../../private/initialize.php'); 
+
+if(isPostRequest()) {
+    $subject = [];
+    $subject['menu_name'] = $_POST['menu_name'] ?? '';
+    $subject['position'] = $_POST['position'] ?? '';
+    $subject['visible'] = $_POST['visible'] ?? '';
+
+    $result = insert_subject($subject);
+    if($result === true) {
+        $new_id = mysqli_insert_id($db);
+        redirect_to(url_for('subjects/show.php?id=' . $new_id)); 
+    } else {
+        $errors = $result;
+    }
+    
+    
+} else {
+    //redirect_to(url_for('subjects/new.php'));
+}
  
 $subject_set = find_all_subjects();
 $subject_count = mysqli_num_rows($subject_set) + 1;
@@ -19,7 +38,8 @@ $subject["position"] = $subject_count
     <div class="subject new">
         <h1>Create Subject</h1>
 
-        <form action="<?php echo url_for('subjects/create.php'); ?>" method="POST">
+        <?php echo display_errors($errors); ?>
+        <form action="<?php echo url_for('subjects/new.php'); ?>" method="POST">
             <dl>
                 <dt>Menu Name</dt>
                 <dd><input type="text" name="menu_name" value="" /></dd>
